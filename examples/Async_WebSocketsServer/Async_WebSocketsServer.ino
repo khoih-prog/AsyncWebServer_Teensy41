4f5238a8df315658720bd843c48ac57b39a4a8af
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2023-01-16 01:20:28 -0500

    Add `Async_WebSocketsServer` example

---
+++ examples/Async_WebSocketsServer/Async_WebSocketsServer.ino
@@ -0,0 +1,229 @@
+/****************************************************************************************************************************
+  Async_WebSocketsServer.ino
+
+  For Teensy41 with QNEthernet
+
+  AsyncWebServer_Teensy41 is a library for the Teensy41 with QNEthernet
+
+  Based on and modified from ESPAsyncWebServer (https://github.com/me-no-dev/ESPAsyncWebServer)
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncWebServer_Teensy41
+  Licensed under GPLv3 license
+ *****************************************************************************************************************************/
+
+//
+// A simple server implementation showing how to:
+//  * serve static messages
+//  * read GET and POST parameters
+//  * handle missing pages / 404s
+//
+
+#if !( defined(CORE_TEENSY) && defined(__IMXRT1062__) && defined(ARDUINO_TEENSY41) )
+  #error Only Teensy 4.1 supported
+#endif
+
+// Debug Level from 0 to 4
+#define _TEENSY41_ASYNC_TCP_LOGLEVEL_       1
+#define _AWS_TEENSY41_LOGLEVEL_             1
+
+#define SHIELD_TYPE     "Teensy4.1 QNEthernet"
+
+#if (_AWS_TEENSY41_LOGLEVEL_ > 3)
+  #warning Using QNEthernet lib for Teensy 4.1. Must also use Teensy Packages Patch or error
+#endif
+
+#define USING_DHCP            true
+//#define USING_DHCP            false
+
+#if !USING_DHCP
+  // Set the static IP address to use if the DHCP fails to assign
+  IPAddress myIP(192, 168, 2, 222);
+  IPAddress myNetmask(255, 255, 255, 0);
+  IPAddress myGW(192, 168, 2, 1);
+  //IPAddress mydnsServer(192, 168, 2, 1);
+  IPAddress mydnsServer(8, 8, 8, 8);
+#endif
+
+#include "QNEthernet.h"       // https://github.com/ssilverman/QNEthernet
+using namespace qindesign::network;
+
+#include <AsyncWebServer_Teensy41.h>
+
+#include "webpage.h"
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len)
+{
+	if (type == WS_EVT_CONNECT)
+	{
+		Serial.printf("ws[Server: %s][ClientID: %u] WSClient connected\n", server->url(), client->id());
+		client->text("Hello from Teensy4_1 Server");
+	}
+	else if (type == WS_EVT_DISCONNECT)
+	{
+		Serial.printf("ws[Server: %s][ClientID: %u] WSClient disconnected\n", server->url(), client->id());
+	}
+	else if (type == WS_EVT_ERROR)
+	{
+		//error was received from the other end
+		Serial.printf("ws[Server: %s][ClientID: %u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+	}
+	else if (type == WS_EVT_PONG)
+	{
+		//pong message was received (in response to a ping request maybe)
+		Serial.printf("ws[Server: %s][ClientID: %u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char*)data : "");
+	}
+	else if (type == WS_EVT_DATA)
+	{
+		//data packet
+		AwsFrameInfo * info = (AwsFrameInfo*)arg;
+
+		if (info->final && info->index == 0 && info->len == len)
+		{
+			//the whole message is in a single frame and we got all of it's data
+			Serial.printf("ws[Server: %s][ClientID: %u] %s-message[len: %llu]: ", server->url(), client->id(),
+			              (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+
+			if (info->opcode == WS_TEXT)
+			{
+				data[len] = 0;
+				Serial.printf("%s\n", (char*)data);
+			}
+			else
+			{
+				for (size_t i = 0; i < info->len; i++)
+				{
+					Serial.printf("%02x ", data[i]);
+				}
+
+				Serial.printf("\n");
+			}
+
+			if (info->opcode == WS_TEXT)
+				client->text("Got your text message");
+			else
+				client->binary("Got your binary message");
+		}
+		else
+		{
+			//message is comprised of multiple frames or the frame is split into multiple packets
+			if (info->index == 0)
+			{
+				if (info->num == 0)
+				{
+					Serial.printf("ws[Server: %s][ClientID: %u] %s-message start\n", server->url(), client->id(),
+					              (info->message_opcode == WS_TEXT) ? "text" : "binary");
+				}
+
+				Serial.printf("ws[Server: %s][ClientID: %u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+			}
+
+			Serial.printf("ws[Server: %s][ClientID: %u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(),
+			              info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+
+			if (info->message_opcode == WS_TEXT)
+			{
+				data[len] = 0;
+				Serial.printf("%s\n", (char*)data);
+			}
+			else
+			{
+				for (size_t i = 0; i < len; i++)
+				{
+					Serial.printf("%02x ", data[i]);
+				}
+
+				Serial.printf("\n");
+			}
+
+			if ((info->index + len) == info->len)
+			{
+				Serial.printf("ws[Server: %s][ClientID: %u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+
+				if (info->final)
+				{
+					Serial.printf("ws[Server: %s][ClientID: %u] %s-message end\n", server->url(), client->id(),
+					              (info->message_opcode == WS_TEXT) ? "text" : "binary");
+
+					if (info->message_opcode == WS_TEXT)
+						client->text("I got your text message");
+					else
+						client->binary("I got your binary message");
+				}
+			}
+		}
+	}
+}
+
+void handleRoot(AsyncWebServerRequest *request)
+{
+	request->send(200, "text/html", webpageCont);
+}
+
+void setup()
+{
+	Serial.begin(115200);
+
+	while (!Serial && millis() < 5000);
+
+	delay(200);
+
+	Serial.print("\nStarting Async_WebSocketsServer on ");
+	Serial.println(BOARD_NAME);
+	Serial.print(" with ");
+  Serial.println(SHIELD_TYPE);
+  Serial.println(ASYNC_WEBSERVER_TEENSY41_VERSION);
+
+	delay(500);
+
+#if USING_DHCP
+  // Start the Ethernet connection, using DHCP
+  Serial.print("Initialize Ethernet using DHCP => ");
+  Ethernet.begin();
+#else
+  // Start the Ethernet connection, using static IP
+  Serial.print("Initialize Ethernet using static IP => ");
+  Ethernet.begin(myIP, myNetmask, myGW);
+  Ethernet.setDNSServerIP(mydnsServer);
+#endif
+
+  if (!Ethernet.waitForLocalIP(5000))
+  {
+    Serial.println(F("Failed to configure Ethernet"));
+
+    if (!Ethernet.linkStatus())
+    {
+      Serial.println(F("Ethernet cable is not connected."));
+    }
+
+    // Stay here forever
+    while (true)
+    {
+      delay(1);
+    }
+  }
+  else
+  {
+    Serial.print(F("Connected! IP address:"));
+    Serial.println(Ethernet.localIP());
+  }
+
+#if USING_DHCP
+  delay(1000);
+#else
+  delay(2000);
+#endif
+
+	///////////////////////////////////
+
+	ws.onEvent(onWsEvent);
+	server.addHandler(&ws);
+
+	server.on("/", handleRoot);
+	server.begin();
+}
+
+void loop()
+{
+}
